5fb11175882d5bd557605a772d75fe9bfeda91a2
Shubham Thanki <156124055+ShubhamThanki@users.noreply.github.com>
2024-06-07 15:35:51 -0400

    Update Data Cleaning P2.sql

--- Data Cleaning P2.sql
+++ Data Cleaning P2.sql
@@ -1,29 +1,49 @@
-DELETE 
-FROM layoff_duplicate3
-WHERE r_n > 1;
-;
-
 SELECT * 
-from layoff_duplicate3
-WHERE industry LIKE '%Crypto%'
+FROM layoff_duplicate3
 ;
 
 UPDATE layoff_duplicate3
-SET company = trim(company);
+SET date = STR_TO_DATE(`date`, '%y-%d-%m');
 
+ALTER table layoff_duplicate3
+MODIFY COLUMN `date` DATE ;
 
-UPDATE layoff_duplicate3
-SET industry = 'Crypto'
-WHERE industry like '%crypto%'
-;
+SELECT  sum(total_laid_off) , YEAR(date)
+FROM layoff_duplicate3
+group by YEAR(date)
+order by 2 desc;
 
-UPDATE layoff_duplicate3
-SET country = 'United States'
-WHERE country = '%United States%'
-;
+SELECT MIN(date) , max(date)
+from layoff_duplicate3;
 
-SELECT `date`
+SELECT SUBSTRING(date , 6,2) AS MONTHS
 FROM layoff_duplicate3;
 
-UPDATE layoff_duplicate3
-SET date = STR_TO_DATE(`date`, '%d%m%Y');
+SELECT SUBSTRING(date, 1,7) AS 'MONTHS' , sum(total_laid_off)
+FROM layoff_duplicate3
+WHERE SUBSTRING(date, 1,7) IS NOT NULL 
+group by SUBSTRING(date, 1,7)
+order by 1 ASC ;
+
+WITH Rolling_total AS 
+(
+SELECT SUBSTRING(date, 1,7) AS MONTHS , sum(total_laid_off) AS total_off
+FROM layoff_duplicate3
+WHERE SUBSTRING(date, 1,7) IS NOT NULL 
+group by SUBSTRING(date, 1,7)
+order by 1 ASC 
+)
+SELECT MONTHS , total_off , SUM(total_off) OVER(ORDER BY MONTHS) AS rolling_total
+FROM Rolling_total
+;
+
+WITH Company_rankings (company , years , total_laid_off) AS 
+(
+SELECT company , YEAR(date) , SUM(total_laid_off)
+FROM layoff_duplicate3
+GROUP BY company , YEAR(date)
+)
+SELECT * , dense_rank() OVER (partition by years ORDER BY total_laid_off DESC) AS RANKS
+FROM company_rankings
+WHERE years IS NOT NULL
+ORDER BY RANKS ASC;
